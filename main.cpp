525e21b9eeb98808e2206b36fc20afb5c6488ab1
Theal Altake <zufa.altake@gmail.com>
2025-12-05 18:28:41 +0200

    removed src

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(){
   GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
   if(window == NULL){
     std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
+    glfwTerminate();//
     return -1;
   }
 
