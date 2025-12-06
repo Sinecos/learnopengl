05ab9b2b85552106ac36c5f2515d050bfde7dbd7
Theal Altake <zufa.altake@gmail.com>
2025-12-06 11:37:37 +0200

    vertex shader and VBO added

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const char *vertexShaderSource = "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "}\0";
+
+
 int main(){
 
   glfwInit();
@@ -33,6 +41,38 @@ int main(){
   //tell GLFW to call this function every window resize
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+  //Normalized Device Coordinates (NDC) space, because glViewport!
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  };
+
+  //First we put the data to Graphics Card Memory (VBO)
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  //Second we create vertex shader and unique ID
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  // attach shader source code to shader object
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  // compile the shader
+  glCompileShader(vertexShader);
+
+  //Check if there is error in compiling the shader
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if(!success){
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILDED\n" << infoLog << std::endl;
+  }
+  
   // render loop
   while(!glfwWindowShouldClose(window)){
 
